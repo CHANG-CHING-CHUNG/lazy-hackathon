815a8eea384f20acf12abc32271653a41b355c84
CHANG-CHING-CHUNG <54635661+CHANG-CHING-CHUNG@users.noreply.github.com>
2020-11-01 11:46:17 +0800

    css purified

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
-const gulp = require('gulp'),
-    gulpImagemin = require('gulp-imagemin');
+const gulp = require('gulp')
+//     gulpImagemin = require('gulp-imagemin');
 
-gulp.task('image', async function () {
-    gulp.src('image/original/**')
-        .pipe(gulpImagemin())
-        .pipe(gulp.dest('image'));
+// gulp.task('image', async function () {
+//     gulp.src('image/original/**')
+//         .pipe(gulpImagemin())
+//         .pipe(gulp.dest('image'));
+// });
+
+const purify = require('gulp-purifycss');
+
+gulp.task('css', function() {
+  return gulp.src('./js/slick/*.css')
+    .pipe(purify(['./js/*.js', './*.html']))
+    .pipe(gulp.dest('./dist/'));
 });
